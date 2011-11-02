f6e1c206acdae6e9f0b99e65bbd2b099b7dd10db
Woxxy <woxxy@foolrulez.org>
2011-11-02 14:32:12 +0100

    Updating generator meta

--- content/themes/default/views/layouts/chan.php
+++ content/themes/default/views/layouts/chan.php
@@ -24,7 +24,7 @@
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo site_url() ?>rss.xml" />
 		<link rel="alternate" type="application/atom+xml" title="Atom" href="<?php echo site_url() ?>atom.xml" />
 		<link rel='index' title='<?php echo get_setting('fs_gen_site_title') ?>' href='<?php echo site_url() ?>' />
-		<meta name="generator" content="FoOlSlide <?php echo FOOLSLIDE_VERSION ?>" />
+		<meta name="generator" content="<?php echo FOOLSLIDE_NAME ?> <?php echo FOOLSLIDE_VERSION ?>" />
 		<?php echo get_setting('fs_theme_header_code'); ?>
 
 	</head>

ab50207137023f4bae54dfd50c77b98ede51a17b
Woxxy <woxxy@foolrulez.org>
2011-12-04 17:21:26 +0100

    Cosmetics for images

--- content/themes/default/views/board.php
+++ content/themes/default/views/board.php
@@ -70,55 +70,42 @@ foreach ($posts as $key => $post) : ?>
 		if ($p->parent == 0)
 			$p->parent = $p->num;
 		?>
-		<?php if ($p->subnum > 0) : ?>
-		<article class="post post_ghost<?php echo ((isset($p->report_status) && !is_null($p->report_status))?' reported':'') ?>" id="<?php echo $p->num . '_' . $p->subnum ?>">
+		
+		<article class="post <?php if ($p->subnum > 0) : ?>post_ghost<?php endif; ?><?php echo ((isset($p->report_status) && !is_null($p->report_status))?' reported':'') ?>" id="<?php echo $p->num . (($p->subnum > 0)?'_' . $p->subnum:'') ?>">
 			<header>
-				<h2 class="post_title"><?php echo $p->title_processed ?></h2>
-				<span class="post_author"><?php echo (($p->email_processed) ? '<a href="mailto:' . form_prep($p->email_processed) . '">' .$p->name_processed.'</a>' : $p->name_processed) ?></span>				<span class="post_trip"><?php echo $p->trip_processed ?></span>
-				<?php if($p->capcode == 'M') : ?>
-					<span class="post_level post_level_moderator">## Mod</span>
-				<?php endif ?>
-				<?php if($p->capcode == 'G') : ?>
-					<span class="post_level post_level_global_moderator">## Global Mod</span>
-				<?php endif ?>
-				<?php if($p->capcode == 'A') : ?>
-					<span class="post_level post_level_administrator">## Admin</span>
-				<?php endif ?>
-				<time datetime="<?php echo date(DATE_W3C, $p->timestamp) ?>"><?php echo date('D M d H:i:s Y', $p->timestamp) ?></time>
-				<span class="post_number"><a href="<?php echo site_url($this->fu_board . '/thread/' . $p->parent) . '#' . $p->num . '_' . $p->subnum ?>" rel="highlight" id="<?php echo $p->num . '_' . $p->subnum ?>">No.</a><a href="<?php echo site_url($this->fu_board . '/thread/' . $p->parent) . '#q' . $p->num . '_' . $p->subnum ?>" rel="quote" id="<?php echo $p->num . ',' . $p->subnum ?>"><?php echo $p->num . ',' . $p->subnum ?></a></span>
-				<?php if (isset($thread_id)) : ?>
-				<span class="post_controls"><a href="<?php echo site_url($this->fu_board . '/report/' . $p->doc_id) ?>" class="btn" rel="report" id="<?php echo $p->doc_id ?>" alt="<?php echo $p->num . ',' . $p->subnum ?>" data-controls-modal="post_tools_report" data-backdrop="true" data-keyboard="true">Report</a> <a href="<?php echo site_url($this->fu_board . '/delete/' . $p->doc_id) ?>" class="btn" rel="delete" id="<?php echo $p->doc_id ?>" alt="<?php echo $p->num . ',' . $p->subnum ?>" data-controls-modal="post_tools_delete" data-backdrop="true" data-keyboard="true">Delete</a></span>
-				<?php endif; ?>
-				<span class="post_type"><img src="<?php echo icons(356, 16) ?>" title="This is a ghost post, not coming from 4chan"/></span>
-		<?php else : ?>
-		<article class="post<?php echo ((isset($p->report_status) && !is_null($p->report_status))?' reported':'') ?>" id="<?php echo $p->num ?>">
-			<header>
-				<h2 class="post_title"><?php echo $p->title_processed ?></h2>
-				<span class="post_author"><?php echo (($p->email_processed) ? '<a href="mailto:' . form_prep($p->email_processed) . '">' .$p->name_processed.'</a>' : $p->name_processed) ?></span>
-				<span class="post_trip"><?php echo $p->trip_processed ?></span>
-				<?php if($p->capcode == 'M') : ?>
-					<span class="post_level post_level_moderator">## Mod</span>
-				<?php endif ?>
-				<?php if($p->capcode == 'G') : ?>
-					<span class="post_level post_level_global_moderator">## Global Mod</span>
-				<?php endif ?>
-				<?php if($p->capcode == 'A') : ?>
-					<span class="post_level post_level_administrator">## Admin</span>
-				<?php endif ?>
-				<time datetime="<?php echo date(DATE_W3C, $p->timestamp) ?>"><?php echo date('D M d H:i:s Y', $p->timestamp) ?></time>
-				<span class="post_number"><a href="<?php echo site_url($this->fu_board . '/thread/' . $p->parent) . '#' . $p->num ?>" rel="highlight" id="<?php echo $p->num ?>">No.</a><a href="<?php echo site_url($this->fu_board . '/thread/' . $p->parent) . '#q' . $p->num ?>" rel="quote" id="<?php echo $p->num ?>"><?php echo $p->num ?></a></span>
-				<?php if (isset($thread_id)) : ?>
-				<span class="post_sub_controls"><a href="<?php echo site_url($this->fu_board . '/report/' . $p->doc_id) ?>" class="btn" rel="report" id="<?php echo $op->doc_id ?>" alt="<?php echo $p->num ?>" data-controls-modal="post_tools_report" data-backdrop="true" data-keyboard="true">Report</a> <a href="<?php echo site_url($this->fu_board . '/delete/' . $p->doc_id) ?>" class="btn" rel="delete" id="<?php echo $p->doc_id ?>" alt="<?php echo $p->num ?>" data-controls-modal="post_tools_delete" data-backdrop="true" data-keyboard="true">Delete</a></span>
-				<?php endif; ?>
-		<?php endif; ?>
-				<?php if ($p->media_filename) : ?>
-				<br/>
-				<span class="post_file">File: <?php echo byte_format($p->media_size, 0) . ', ' . $p->media_w . 'x' . $p->media_h . ', ' . $p->media ?></span>
-				<span class="post_file_controls">[<a href="<?php echo site_url($this->fu_board . '/image/' . urlencode(substr($p->media_hash, 0, -2))) ?>">View Same</a>] [<a href="http://iqdb.org/?url=<?php echo $p->thumbnail_href ?>">iqdb</a>] [<a href="http://google.com/searchbyimage?image_url=<?php echo $p->thumbnail_href ?>">Google</a>]</span>
-				<?php endif; ?>
+				<div class="post_data">
+					<h2 class="post_title"><?php echo $p->title_processed ?></h2>
+					<span class="post_author"><?php echo (($p->email_processed) ? '<a href="mailto:' . form_prep($p->email_processed) . '">' .$p->name_processed.'</a>' : $p->name_processed) ?></span> <span class="post_trip"><?php echo $p->trip_processed ?></span>
+					<?php if($p->capcode == 'M') : ?>
+						<span class="post_level post_level_moderator">## Mod</span>
+					<?php endif ?>
+					<?php if($p->capcode == 'G') : ?>
+						<span class="post_level post_level_global_moderator">## Global Mod</span>
+					<?php endif ?>
+					<?php if($p->capcode == 'A') : ?>
+						<span class="post_level post_level_administrator">## Admin</span>
+					<?php endif ?>
+					<time datetime="<?php echo date(DATE_W3C, $p->timestamp) ?>"><?php echo date('D M d H:i:s Y', $p->timestamp) ?></time>
+					<span class="post_number"><a href="<?php echo site_url($this->fu_board . '/thread/' . $p->parent) . '#' . $p->num . (($p->subnum > 0)?'_' . $p->subnum:'') ?>" rel="highlight" id="<?php echo $p->num . (($p->subnum > 0)?'_' . $p->subnum:'') ?>">No.</a><a href="<?php echo site_url($this->fu_board . '/thread/' . $p->parent) . '#q' . $p->num . (($p->subnum > 0)?'_' . $p->subnum:'') ?>" rel="quote" id="<?php echo $p->num . (($p->subnum > 0)?',' . $p->subnum:'') ?>"><?php echo $p->num . (($p->subnum > 0)?',' . $p->subnum:'') ?></a></span>
+					<?php if (isset($thread_id)) : ?>
+						<span class="post_controls">
+							<a href="<?php echo site_url($this->fu_board . '/report/' . $p->doc_id) ?>" class="btn parent" rel="report" id="<?php echo $p->doc_id ?>" alt="<?php echo $p->num . (($p->subnum > 0)?',' . $p->subnum:'') ?>" data-controls-modal="post_tools_report" data-backdrop="true" data-keyboard="true">Report</a><a href="<?php echo site_url($this->fu_board . '/delete/' . $p->doc_id) ?>" class="btn parent" rel="delete" id="<?php echo $p->doc_id ?>" alt="<?php echo $p->num . (($p->subnum > 0)?',' . $p->subnum:'') ?>" data-controls-modal="post_tools_delete" data-backdrop="true" data-keyboard="true">Delete</a>
+						</span>
+					<?php endif; ?>
+				</div>
+				<?php if ($p->subnum > 0) : ?><span class="post_type"><img src="<?php echo icons(356, 16) ?>" title="This is a ghost post, not coming from 4chan"/></span><?php endif ?>
 			</header>
 		<?php if ($p->media_filename) : ?>
-		<a href="<?php echo $p->remote_image_href ?>" target="_blank" class="thread_image_link"><img src="<?php echo $p->thumbnail_href ?>" width="<?php echo $p->preview_w ?>" height="<?php echo $p->preview_h ?>" md5="<?php echo $p->media_hash ?>" class="post_image" /></a>
+		<div class="thread_image_box">
+			<span class="post_file"><?php echo $p->media ?></span>
+			<br/>
+			<a href="<?php echo $p->remote_image_href ?>" target="_blank" class="thread_image_link"><img src="<?php echo $p->thumbnail_href ?>" width="<?php echo $p->preview_w ?>" height="<?php echo $p->preview_h ?>" md5="<?php echo $p->media_hash ?>" class="post_image" /></a>
+			<br/>
+			<span class="post_file"><?php echo byte_format($p->media_size, 0) . ', ' . $p->media_w . 'x' . $p->media_h ?></span>
+			<div class="post_file_controls">
+				<a href="<?php echo site_url($this->fu_board . '/image/' . urlencode(substr($p->media_hash, 0, -2))) ?>" class="btn parent">View Same</a><a href="http://iqdb.org/?url=<?php echo $p->thumbnail_href ?>" class="btn parent">iqdb</a><a href="http://google.com/searchbyimage?image_url=<?php echo $p->thumbnail_href ?>" class="btn parent">Google</a>
+			</div>
+		</div>
 		<?php endif; ?>
 			<div class="text">
 				<?php echo $p->comment_processed ?>

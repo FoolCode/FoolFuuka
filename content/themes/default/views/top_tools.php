b21b9042c00c3d5955179f364b2e5c8863e9694f
Woxxy <woxxy@foolrulez.org>
2011-12-13 13:47:29 +0100

    Fixed the search bar style

--- content/themes/default/views/top_tools.php
+++ content/themes/default/views/top_tools.php
@@ -3,6 +3,69 @@ if (!isset($page))
 	$page = 1;
 ?>
 
+<!--- Post Input -->
+<div style="">
+<?php
+echo form_open($this->fu_board . '/post');
+echo '<div class="input-prepend">';
+echo '<span class="add-on">Post No.</span>';
+echo form_input(array(
+	'name' => 'post',
+	'id' => 'post',
+	'class' => 'mini'
+));
+echo form_submit(array(
+	'value' => 'Go',
+	'class' => 'btn notice',
+	'style' => 'margin-left: -2px',
+	'onClick' => 'getPost(this.form); return false;'
+));
+echo '</div>';
+echo form_close();
+?>
+</div>
+
+
+<!--- Page Input -->
+<div>
+<?php
+if (!$this->input->cookie('fu_ghost_mode'))
+{
+	$toggle_mode = 'ghost';
+	echo form_open($this->fu_board . '/page');
+	echo '<div class="input-prepend">';
+	echo '<span class="add-on">Page #</span>';
+}
+else
+{
+	$toggle_mode = 'page';
+	echo form_open($this->fu_board . '/ghost');
+	echo '<div class="input-prepend">';
+	echo '<span class="add-on">? Page #</span>';
+	
+}
+echo form_input(array(
+	'name' => 'page',
+	'id' => 'page',
+	'class' => 'mini',
+	'value' => $page
+));
+echo form_submit(array(
+	'value' => 'Go',
+	'class' => 'btn notice',
+	'style' => 'margin-left: -2px; border-radius:0; -moz-border-radius:0; -webkit-border-radius:0;',
+	'onClick' => 'getPage(this.form); return false;'
+));
+?>
+	
+<input type="button" class="btn notice<?php echo ($this->input->cookie('fu_ghost_mode')) ? ' active' : '' ?>" style="margin-left:-5px;" value="Ghost Mode" onClick="location.href='<?php echo site_url($this->fu_board . '/' . $toggle_mode . '/' . $page) ?>'">
+<?php
+echo '</div>';
+echo form_close();
+?>
+</div>	
+
+
 <!--- Search Input -->
 <div id="search_simple">
 <?php
@@ -12,18 +75,19 @@ echo '<span class="add-on" rel="popover" data-original-title="How to search" dat
 echo form_input(array(
 	'name' => 'text',
 	'id' => 'text',
+	'style' => 'width:239px',
 	'value' => (isset($search["text"])) ? rawurldecode($search["text"]) : ''
 ));
 echo form_submit(array(
 	'value' => 'Search',
 	'class' => 'btn notice',
-	'style' => 'border-radius:0; -webkit-border-radius:0; -moz-border-radius:0',
+	'style' => 'margin-left: -2px; border-radius:0; -moz-border-radius:0; -webkit-border-radius:0;',
 	'onClick' => 'getSearch(\'simple\', this.form); return false;'
 ));
 echo form_submit(array(
 	'value' => 'Advanced',
 	'class' => 'btn notice',
-	'style' => 'margin-left:-6px',
+	'style' => 'margin-left: -1px;',
 	'onClick' => 'toggleSearch(\'simple\'); toggleSearch(\'advanced\'); return false;'
 ));
 echo '</div>';
@@ -36,22 +100,23 @@ echo form_close();
 <?php
 echo form_open($this->fu_board . '/search');
 echo '<div class="input-prepend">';
-echo '<span class="add-on" rel="popover-right" data-original-title="How to search" data-content="' . htmlentities('Place a <tt>|</tt> in between expressions to get one of them in results, e.g. <tt>tripcode|email</tt> to locate posts that contain either the word tripcode or email in them.<br />Place a <tt>-</tt> before a word to exclude posts containing that word: <tt>-tripcode</tt><br />Place quotes around phrases to find pages containing the phrase: <tt>"I am a filthy tripcode user"</tt>') . '">?</span>';
+echo '<span class="add-on" rel="popover-below" data-original-title="How to search" data-content="' . htmlentities('Place a <tt>|</tt> in between expressions to get one of them in results, e.g. <tt>tripcode|email</tt> to locate posts that contain either the word tripcode or email in them.<br />Place a <tt>-</tt> before a word to exclude posts containing that word: <tt>-tripcode</tt><br />Place quotes around phrases to find pages containing the phrase: <tt>"I am a filthy tripcode user"</tt>') . '">?</span>';
 echo form_input(array(
 	'name' => 'text',
 	'id' => 'text',
+	'style' => 'width:239px',
 	'value' => (isset($search["text"])) ? rawurldecode($search["text"]) : ''
 ));
 echo form_submit(array(
 	'value' => 'Search',
 	'class' => 'btn notice',
-	'style' => 'border-radius:0; -webkit-border-radius:0; -moz-border-radius:0',
+	'style' => 'margin-left: -2px; border-radius:0; -moz-border-radius:0; -webkit-border-radius:0;',
 	'onClick' => 'getSearch(\'advanced\', this.form); return false;'
 ));
 echo form_submit(array(
 	'value' => 'Advanced',
 	'class' => 'btn notice active',
-	'style' => 'margin-left:-6px',
+	'style' => 'margin-left: -1px;',
 	'onClick' => 'toggleSearch(\'simple\'); toggleSearch(\'advanced\'); return false;'
 ));
 echo '</div>';
@@ -142,59 +207,4 @@ echo '</div>';
 </div>
 <?php echo form_close(); ?>
 </div>
-
-<!--- Post Input -->
-<?php
-echo form_open($this->fu_board . '/post');
-echo '<div class="input-prepend">';
-echo '<span class="add-on">Post No.</span>';
-echo form_input(array(
-	'name' => 'post',
-	'id' => 'post',
-	'class' => 'mini'
-));
-echo form_submit(array(
-	'value' => 'Go',
-	'class' => 'btn notice',
-	'onClick' => 'getPost(this.form); return false;'
-));
-echo '</div>';
-echo form_close();
-?>
-
-<!--- Page Input -->
-<?php
-if (!$this->input->cookie('fu_ghost_mode'))
-{
-	$toggle_mode = 'ghost';
-	echo form_open($this->fu_board . '/page');
-	echo '<div class="input-prepend">';
-	echo '<span class="add-on">Page #</span>';
-}
-else
-{
-	$toggle_mode = 'page';
-	echo form_open($this->fu_board . '/ghost');
-	echo '<div class="input-prepend">';
-	echo '<span class="add-on">? Page #</span>';
-	
-}
-echo form_input(array(
-	'name' => 'page',
-	'id' => 'page',
-	'class' => 'mini',
-	'value' => $page
-));
-echo form_submit(array(
-	'value' => 'Go',
-	'class' => 'btn notice',
-	'style' => 'border-radius:0; -webkit-border-radius:0; -moz-border-radius:0',
-	'onClick' => 'getPage(this.form); return false;'
-));
-?>
-<input type="button" class="btn notice<?php echo ($this->input->cookie('fu_ghost_mode')) ? ' active' : '' ?>" style="margin-left: -6px" value="Ghost Mode" onClick="location.href='<?php echo site_url($this->fu_board . '/' . $toggle_mode . '/' . $page) ?>'">
-<?php
-echo '</div>';
-echo form_close();
-?>
 <div class="clearfix"></div>

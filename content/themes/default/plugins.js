550638d635929785b27dfef2e5f4e8f259b93dd6
Woxxy <woxxy@foolrulez.org>
2011-12-20 01:50:18 +0100

    Autoinput password in deletion request

--- content/themes/default/plugins.js
+++ content/themes/default/plugins.js
@@ -26,6 +26,7 @@ jQuery(document).ready(function() {
 				break;
 				
 			case 'delete':
+				var foolfuuka_reply_password = getCookie('foolfuuka_reply_password');
 				modal.find(".title").html('Delete - Post No. ' + el.data("post-id"));
 				modal.find(".modal-loading").hide();
 				modal.find(".modal-information").html('\
@@ -33,6 +34,10 @@ jQuery(document).ready(function() {
 					<input type="password" class="modal-password" />\n\
 					<input type="hidden" class="modal-post-id" value="' + post + '" />');
 				modal.find(".submitModal").data("action", 'delete');
+				if(foolfuuka_reply_password != null)
+				{
+					modal.find(".modal-password").val(foolfuuka_reply_password);
+				}
 				break;
 			
 			case 'closeModal':
@@ -222,4 +227,45 @@ function toggleHighlight(id, classn, single)
 function replyHighlight(id)
 {
 	toggleHighlight(id, 'highlight', true);
+}
+
+
+function getCookie( check_name ) {
+	// first we'll split this cookie up into name/value pairs
+	// note: document.cookie only returns name=value, not the other components
+	var a_all_cookies = document.cookie.split( ';' );
+	var a_temp_cookie = '';
+	var cookie_name = '';
+	var cookie_value = '';
+	var b_cookie_found = false; // set boolean t/f default f
+
+	for ( i = 0; i < a_all_cookies.length; i++ )
+	{
+		// now we'll split apart each name=value pair
+		a_temp_cookie = a_all_cookies[i].split( '=' );
+
+
+		// and trim left/right whitespace while we're at it
+		cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
+
+		// if the extracted name matches passed check_name
+		if ( cookie_name == check_name )
+		{
+			b_cookie_found = true;
+			// we need to handle case where cookie has no value but exists (no = sign, that is):
+			if ( a_temp_cookie.length > 1 )
+			{
+				cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
+			}
+			// note that in cases where cookie is initialized but no value, null is returned
+			return cookie_value;
+			break;
+		}
+		a_temp_cookie = null;
+		cookie_name = '';
+	}
+	if ( !b_cookie_found )
+	{
+		return null;
+	}
 }

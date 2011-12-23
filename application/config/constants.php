48f7160c3e507349e29419efc4755449efc6e9e4
Woxxy <woxxy@foolrulez.org>
2011-12-23 13:30:11 +0100

    Changing post image layout

--- application/config/constants.php
+++ application/config/constants.php
@@ -45,7 +45,7 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 |
 */
 
-define('FOOLSLIDE_VERSION', '0.2.1');
+define('FOOLSLIDE_VERSION', '0.2.2');
 define('FOOLSLIDE_NAME', 'FoOlFuuka');
 define('FOOLSLIDE_MANUAL_INSTALL_URL', 'http://ask.foolrulez.com');
 

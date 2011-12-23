368395c6380b3d5186893ad63a64968966b2779c
Woxxy <woxxy@foolrulez.org>
2011-12-23 23:37:49 +0100

    Added saucenao

--- content/themes/default/theme_functions.php
+++ content/themes/default/theme_functions.php
@@ -21,9 +21,9 @@ function build_board_comment($p, $modifiers = array()) {
 						<?php if ($p->media_filename) : ?>
 							<div class="post_file">
 								<span class="post_file_controls">
-									<a href="<?php echo site_url($CI->fu_board . '/image/' . urlencode(substr($p->media_hash, 0, -2))) ?>" class="btnr parent">View Same</a><a target="_blank" href="http://iqdb.org/?url=<?php echo $p->thumbnail_href ?>" class="btnr parent">iqdb</a><a target="_blank" href="http://google.com/searchbyimage?image_url=<?php echo $p->thumbnail_href ?>" class="btnr parent">Google</a>
+									<a href="<?php echo site_url($CI->fu_board . '/image/' . urlencode(substr($p->media_hash, 0, -2))) ?>" class="btnr parent">View Same</a><a target="_blank" href="http://iqdb.org/?url=<?php echo $p->thumbnail_href ?>" class="btnr parent">iqdb</a><a target="_blank" href="http://saucenao.com/search.php?url=<?php echo $p->thumbnail_href ?>" class="btnr parent">SauceNAO</a><a target="_blank" href="http://google.com/searchbyimage?image_url=<?php echo $p->thumbnail_href ?>" class="btnr parent">Google</a>
 								</span>
-								<?php echo $p->media ?>,
+								<span class="post_file_filename"><?php echo $p->media ?></span>,
 								<?php echo byte_format($p->media_size, 0) . ', ' . $p->media_w . 'x' . $p->media_h ?>
 							</div>
 							<div class="thread_image_box">

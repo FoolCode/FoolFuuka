713e62e99f6c764f90cb6cb9f780ac76e04ec09d
Woxxy <woxxy@foolrulez.org>
2011-12-28 15:50:29 +0100

    Fixed change theme javascript cookie on /

--- content/themes/fuuka/plugins.js
+++ content/themes/fuuka/plugins.js
@@ -57,15 +57,30 @@ window.onload = function() {
 }
 
 
-
-function setCookie(c_name,value,exdays)
+function setCookie( name, value, expires, path, domain, secure )
 {
-	var exdate=new Date();
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-	document.cookie=c_name + "=" + c_value;
-}
+// set time, it's in milliseconds
+var today = new Date();
+today.setTime( today.getTime() );
 
+/*
+if the expires variable is set, make the correct
+expires time, the current script below will set
+it for x number of days, to make it for hours,
+delete * 24, for minutes, delete * 60 * 24
+*/
+if ( expires )
+{
+expires = expires * 1000 * 60 * 60 * 24;
+}
+var expires_date = new Date( today.getTime() + (expires) );
+
+document.cookie = name + "=" +escape( value ) +
+( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
+( ( path ) ? ";path=" + path : "" ) +
+( ( domain ) ? ";domain=" + domain : "" ) +
+( ( secure ) ? ";secure" : "" );
+}
 
 function getCookie( check_name ) {
 	// first we'll split this cookie up into name/value pairs
@@ -110,6 +125,6 @@ function getCookie( check_name ) {
 
 var changeTheme = function(theme)
 {
-	setCookie('foolfuuka_theme', theme, 30);
+	setCookie('foolfuuka_theme', theme, 30, '/');
 	window.location.reload();
 }

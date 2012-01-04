3528d188cc7abb4d836c275a700dab3a609cf212
Hung Tran <oohnoitz@gmail.com>
2012-01-04 04:19:00 -0600

    Fixing yotsuba theme.

--- content/themes/yotsuba/views/board.php
+++ content/themes/yotsuba/views/board.php
@@ -16,10 +16,10 @@ foreach ($posts as $key => $post) : ?>
 	?>
 		<?php if ($op->media_filename) : ?>
 		<span class="filesize">
-			File : <a href="<?php echo ($op->image_href) ? $op->image_href : $op->remote_image_href ?>" rel="noreferrer" target="_blank"><?php echo $op->media_filename ?></a><?php echo '-(' . byte_format($op->media_size, 0) . ', ' . $op->media_w . 'x' . $op->media_h . ')' ?>
+			File : <a href="<?php echo ($op->image_href) ? $op->image_href : $op->remote_image_href ?>" target="_blank"><?php echo $op->media_filename ?></a><?php echo '-(' . byte_format($op->media_size, 0) . ', ' . $op->media_w . 'x' . $op->media_h . ')' ?>
 		</span>
 		<br>
-		<a href="<?php echo ($op->image_href) ? $op->image_href : $op->remote_image_href ?>" rel="noreferrer" target="_blank">
+		<a href="<?php echo ($op->image_href) ? $op->image_href : $op->remote_image_href ?>" target="_blank">
 			<img src="<?php echo $op->thumbnail_href ?>" border="0" align="left" width="<?php echo $op->preview_w ?>" height="<?php echo $op->preview_h ?>" hspace="20" alt="<?php echo byte_format($op->media_size, 0) ?>" md5="<?php echo $op->media_hash ?>"/>
 		</a>
 		<?php endif; ?>
@@ -27,9 +27,18 @@ foreach ($posts as $key => $post) : ?>
 		<a name="0"></a>
 
 		<input type="checkbox" name="delete[]" value="<?php echo $op->doc_id ?>"/>
-		<span class="filetitle"></span>
-		<span class="postername"><?php echo $op->name ?></span>
-		<span class="postertrip"><?php echo $op->trip ?></span>
+		<span class="filetitle"><?php echo $op->title_processed ?></span>
+		<span class="postername"><?php echo (($op->email_processed && $op->email_processed != 'noko') ? '<a href="mailto:' . form_prep($op->email_processed) . '">' . $op->name_processed . '</a>' : $op->name_processed) ?></span>
+		<span class="postertrip"><?php echo $op->trip_processed ?></span>
+		<?php if ($op->capcode == 'M') : ?>
+			<span class="post_level_moderator">## Mod</span>
+		<?php endif ?>
+		<?php if ($op->capcode == 'G') : ?>
+			<span class="post_level_global_moderator">## Global Mod</span>
+		<?php endif ?>
+		<?php if ($op->capcode == 'A') : ?>
+			<span class="post_level_administrator">## Admin</span>
+		<?php endif ?>
 		<span class="posttime"><?php echo date('m/d/y(D)H:i', $op->timestamp + 18000) ?></span>
 		<span id="nothread<?php echo $op->num ?>">
 			<a href="<?php echo site_url($this->fu_board . '/thread/' . $op->num) . '#' . $op->num ?>" class="quotejs">No.</a><a href="<?php echo site_url($this->fu_board . '/thread/' . $op->num) . '#q' . $op->num ?>" class="quotejs"><?php echo $op->num ?></a> &nbsp; [<a href="<?php echo site_url($this->fu_board . '/thread/' . $op->num) ?>">Reply</a>]
@@ -37,26 +46,14 @@ foreach ($posts as $key => $post) : ?>
 		<blockquote>
 			<?php echo $op->comment_processed ?>
 		</blockquote>
-		<?php echo ((isset($post['omitted']) && $post['omitted'] > 0) ? '<span class="omittedposts">' . $post['omitted'] . ' posts '.((isset($post['images_omitted']) && $post['images_omitted'] > 0)?'and '.$post['images_omitted'].' images':'').' replies omitted. Click Reply to view.</span>' : '') ?>
+		<?php echo ((isset($post['omitted']) && $post['omitted'] > 0) ? '<span class="omittedposts">' . $post['omitted'] . ' posts '.((isset($post['images_omitted']) && $post['images_omitted'] > 0)?' and '.$post['images_omitted'].' images replies ':'') . 'omitted. Click Reply to view.</span>' : '') ?>
 	<?php endif; ?>
 
 	<?php
 	if (isset($post['posts']))
 	{
-		if (isset($posts_per_thread))
-		{
-			$limit = count($post['posts']) - $posts_per_thread;
-			if ($limit < 0)
-				$limit = 0;
-		}
-		else
-		{
-			$limit = 0;
-		}
 
-		for ($i = $limit; $i < count($post['posts']); $i++)
-		{
-			$p = $post['posts'][$i];
+		foreach ($post['posts'] as $p) :
 
 			if ($p->parent == 0)
 				$p->parent = $p->num;
@@ -69,7 +66,9 @@ foreach ($posts as $key => $post) : ?>
 			{
 				echo build_board_comment($p, $modifiers);
 			}
-		}
+
+		endforeach;
+		
 	}
 	?>
 	<br clear="left">

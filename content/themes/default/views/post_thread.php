097064499a3cf3f8f79b56c275b6f60e8fbbc837
Woxxy <woxxy@foolrulez.org>
2012-01-18 23:13:57 +0100

    What...

---
+++ content/themes/default/views/post_thread.php
@@ -0,0 +1,114 @@
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+?>
+
+
+<?php if (get_selected_board()->archive && isset($is_page)) : ?>
+	<div>
+		<?php echo form_open_multipart(get_selected_board()->shortname.'/sending') ?>
+		<fieldset>
+			<div class="clearfix">
+				<label for="reply_bokunonome">Name</label>
+				<div class="input">
+					<?php echo form_input(array(
+						'name' => 'name',
+						'id' => 'reply_name_yep',
+						'style' => 'display:none'
+					)); ?>
+					<?php echo form_input(array(
+						'name' => 'reply_bokunonome',
+						'id' => 'reply_bokunonome',
+						'value' => ((isset($this->fu_reply_name))?$this->fu_reply_name:'')
+					)); ?>
+				</div>
+			</div>
+			<div class="clearfix">
+				<label for="reply_elitterae">E-mail</label>
+				<div class="input">
+					<?php echo form_input(array(
+						'name' => 'email',
+						'id' => 'reply_email_yep',
+						'style' => 'display:none'
+					)); ?>
+					<?php echo form_input(array(
+						'name' => 'reply_elitterae',
+						'id' => 'reply_elitterae',
+						'value' => ((isset($this->fu_reply_email))?$this->fu_reply_email:'')
+					)); ?>
+				</div>
+			</div>
+			<div class="clearfix">
+				<label for="reply_talkingde">Subject</label>
+				<div class="input">
+					<?php echo form_input(array(
+						'name' => 'reply_talkingde',
+						'id' => 'reply_talkingde'
+					)); ?>
+				</div>
+			</div>
+			<div class="clearfix">
+				<label for="reply_chennodiscursus">Comment</label>
+				<div class="input">
+					<?php echo form_textarea(array(
+						'name' => 'reply',
+						'id' => 'reply_comment_yep',
+						'style' => 'display:none'
+					)); ?>
+					<?php echo form_textarea(array(
+						'name' => 'reply_chennodiscursus',
+						'id' => 'reply_chennodiscursus',
+						'rows' => 3
+					)); ?>
+				</div>
+			</div>
+			<div class="clearfix">
+				<label for="reply_file">File</label>
+				<div class="input">
+					<?php echo form_upload(array(
+						'name' => 'file_image',
+						'id' => 'file_image'
+					)); ?>
+				</div>
+			</div>
+			<div class="clearfix">
+				<label for="reply_nymphassword">Password</label>
+				<div class="input">
+					<?php echo form_password(array(
+						'name' => 'reply_nymphassword',
+						'id' => 'reply_nymphassword',
+						'value' => $this->fu_reply_password
+					)); ?>
+				</div>
+			</div>
+			<?php
+			// controls for administrators and moderators
+			if($this->tank_auth->is_allowed()) : ?>
+			<div class="clearfix">
+				<label for="reply_postas">Post as</label>
+				<div class="input">
+					<?php
+					$postas = array('user' => 'User', 'mod' => 'Moderator');
+					if($this->tank_auth->is_admin())
+					{
+						$postas['admin'] = 'Administrator';
+					}
+					echo form_dropdown('reply_postas', $postas, 'User', 'id="reply_postas"'); ?>
+				</div>
+			</div>
+			<?php endif; ?>
+			<div class="actions">
+			<?php
+				echo form_hidden('reply_numero', 0);
+				echo form_hidden('MAX_FILE_SIZE', 3072);
+				echo form_submit(array(
+					'name' => 'reply_gattai',
+					'value' => 'Submit',
+					'class' => 'btn primary'
+				));
+			?>
+			</div>
+		</fieldset>
+		<?php echo form_close() ?>
+	</div>
+<?php endif; ?>
---
+++ content/themes/fuuka/views/post_thread.php
@@ -0,0 +1,78 @@
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+?>
+
+<?php if (get_selected_board()->archive && isset($is_page)) : ?>
+<?php echo form_open_multipart(get_selected_board()->shortname .'/sending', array('id' => 'postform')); ?>
+<table style="margin-left: auto; margin-right: auto">
+	<tbody>
+		<tr>
+			<td class="subreply">
+				<div class="theader">
+					Create New Thread
+				</div>
+				<?php if(isset($reply_errors)) : ?>
+				<span style="color:red"><?php echo $reply_errors ?></span>
+				<?php endif; ?>
+				<table>
+					<tbody>
+						<tr>
+							<td class="postblock">Name</td>
+							<td><?php echo form_input(array('name' => 'NAMAE', 'size' => 63, 'value' => ((isset($this->fu_reply_name))?$this->fu_reply_name:''))); ?></td>
+						</tr>
+						<tr>
+							<td class="postblock">E-mail</td>
+							<td><?php echo form_input(array('name' => 'MERU', 'size' => 63, 'value' => ((isset($this->fu_reply_email))?$this->fu_reply_email:''))); ?></td>
+						</tr>
+						<tr>
+							<td class="postblock">Subject</td>
+							<td><?php echo form_input(array('name' => 'subject', 'size' => 63)); ?></td>
+						</tr>
+						<tr>
+							<td class="postblock">Comment</td>
+							<td><?php echo form_textarea(array('name' => 'KOMENTO', 'cols' => 48, 'rows' => 4)); ?></td>
+						</tr>
+						<tr>
+							<td class="postblock">File</td>
+							<td><?php echo form_upload(array('name' => 'file_image', 'id' => 'file_image')); ?></td>
+						</tr>
+						<tr>
+							<td class="postblock">Password <a class="tooltip" href="#">[?] <span>Password used for file deletion.</span></a></td>
+							<td><?php echo form_password(array('name' => 'delpass', 'size' => 24, 'value' => $this->fu_reply_password)); ?></td>
+						</tr>
+						<?php if($this->tank_auth->is_allowed()) : ?>
+						<tr>
+							<td class="postblock">Post As</td>
+							<td>
+								<?php
+								$postas = array('user' => 'User', 'mod' => 'Moderator');
+								if($this->tank_auth->is_admin()) { $postas['admin'] = 'Administrator'; }
+								echo form_dropdown('reply_postas', $postas,'id="reply_postas"');
+								?>
+							</td>
+						</tr>
+						<?php endif; ?>
+						<tr>
+							<td class="postblock">Action</td>
+							<td>
+								<?php
+									echo form_hidden('parent', 0);
+									echo form_hidden('MAX_FILE_SIZE', 3072);
+									echo form_submit(array(
+										'name' => 'reply_action',
+										'value' => 'Submit'
+									));
+								?>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
+<?php echo form_close() ?>
+
+<hr/>
+<?php endif; ?>

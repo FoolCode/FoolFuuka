103be97c1de55a4591d457daaf6942ea53e0552a
Hung Tran <oohnoitz@gmail.com>
2012-02-16 18:16:32 -0600

    Changed check from media to media_filename.

--- content/themes/default/views/board_comment.php
+++ content/themes/default/views/board_comment.php
@@ -4,7 +4,7 @@ if (!defined('BASEPATH'))
 ?>
 
 <article class="post doc_id_<?php echo $p->doc_id ?><?php if ($p->subnum > 0) : ?> post_ghost<?php endif; ?><?php if ($p->parent == $p->num) : ?> post_is_op<?php endif; ?><?php echo ((isset($p->report_status) && !is_null($p->report_status)) ? ' reported' : '') ?><?php echo ($p->media ? ' has_image' : '') ?><?php if ($p->media) : ?> clearfix<?php endif; ?><?php if (false && $p->spam == 1) : ?> is_spam<?php endif; ?>" id="<?php echo $p->num . (($p->subnum > 0) ? '_' . $p->subnum : '') ?>">
-	<?php if ($p->media) : ?>
+	<?php if ($p->media_filename) : ?>
 		<div class="post_file">
 			<span class="post_file_controls">
 				<?php if (get_selected_radix()->thumbnails || $this->tank_auth->is_allowed()) : ?><a href="<?php echo site_url(get_selected_radix()->shortname . '/image/' . substr(urlsafe_b64encode(urlsafe_b64decode($p->media_hash)), 0, -2)) ?>" class="btnr parent">View Same</a><a target="_blank" href="http://iqdb.org/?url=<?php echo $p->thumbnail_href ?>" class="btnr parent">iqdb</a><a target="_blank" href="http://saucenao.com/search.php?url=<?php echo $p->thumbnail_href ?>" class="btnr parent">SauceNAO</a><a target="_blank" href="http://google.com/searchbyimage?image_url=<?php echo $p->thumbnail_href ?>" class="btnr parent">Google</a><?php endif; ?>

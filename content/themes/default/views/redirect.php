6acb4e82833c8a888725ee610821a9d0369842bf
woxxy <woxxy@foolrulez.org>
2012-03-09 21:45:49 +0100

    Solving merge issues

--- content/themes/default/views/redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('BASEPATH'))
-	exit('No direct script access allowed'); ?>
-Redirecting ...
-
-<script type="text/javascript">
-	window.location.href = '<?php echo $url ?>';
-</script>

ec300f9f0f09582e33de4d75493d4e9bc147c41e
woxxy <woxxy@foolrulez.org>
2012-04-26 00:20:58 +0200

    Fixing colors in changelog visualization

--- application/views/admin/system/upgrade.php
+++ application/views/admin/system/upgrade.php
@@ -30,11 +30,11 @@ if ($new_versions)
 	echo '<div class="table" style="padding-bottom: 10px; margin-right:10px;">';
 	echo '<h3>' . _('Changelog') . '</h3><div class="changelog">';
 	$changelog = Markdown($changelog);
-	$changelog = str_replace('{{ENHANCEMENT}}', '<span class="badge badge-warning">Enhancement</span>', $changelog);
-	$changelog = str_replace('{{BUGFIX}}', '<span class="badge badge-info">Bugfix</span>', $changelog);
-	$changelog = str_replace('{{NEW}}', '<span class="badge badge-success">New</span>', $changelog);
-	$changelog = str_replace('{{ATTENTION}}', '<span class="badge badge-warning">Attention</span>', $changelog);
-	$changelog = str_replace('{{NEWS}}', '<span class="badge badge-inverse">News</span>', $changelog);
+	$changelog = str_replace('{{ENHANCEMENT}}', '<span class="label label-warning">Enhancement</span>', $changelog);
+	$changelog = str_replace('{{BUGFIX}}', '<span class="label label-info">Bugfix</span>', $changelog);
+	$changelog = str_replace('{{NEW}}', '<span class="label label-success">New</span>', $changelog);
+	$changelog = str_replace('{{ATTENTION}}', '<span class="label label-important">Attention</span>', $changelog);
+	$changelog = str_replace('{{NEWS}}', '<span class="label label-inverse">News</span>', $changelog);
 	echo $changelog;
 	echo '</div></div>';
 }

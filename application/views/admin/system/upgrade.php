14f489520ce051686d68db4fda67b61b0ac09280
woxxy <woxxy@foolrulez.org>
2012-04-26 00:10:23 +0200

    Refining changelog

--- application/views/admin/system/upgrade.php
+++ application/views/admin/system/upgrade.php
@@ -30,7 +30,11 @@ if ($new_versions)
 	echo '<div class="table" style="padding-bottom: 10px; margin-right:10px;">';
 	echo '<h3>' . _('Changelog') . '</h3><div class="changelog">';
 	$changelog = Markdown($changelog);
-	$changelog = str_replace('{{Enhancement}}', '<span class="pill pill-alert">Enhancement</span>', $changelog);
+	$changelog = str_replace('{{ENHANCEMENT}}', '<span class="badge badge-warning">Enhancement</span>', $changelog);
+	$changelog = str_replace('{{BUGFIX}}', '<span class="badge badge-warning">Bugfix</span>', $changelog);
+	$changelog = str_replace('{{NEW}}', '<span class="badge badge-success">New</span>', $changelog);
+	$changelog = str_replace('{{ATTENTION}}', '<span class="badge badge-warning">Attention</span>', $changelog);
+	$changelog = str_replace('{{NEWS}}', '<span class="badge badge-inverse">News</span>', $changelog);
 	echo $changelog;
 	echo '</div></div>';
 }

550b484dc9e0071dd3c8d7930491dfe33fefac03
woxxy <woxxy@foolrulez.org>
2012-04-25 23:34:01 +0200

    Removing php-gettext

--- application/views/admin/system/upgrade.php
+++ application/views/admin/system/upgrade.php
@@ -3,16 +3,11 @@ if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 ?>
 
-<div class="table" style="padding-bottom: 10px">
-	<h3 style="float: left"><?php echo _('Upgrade'); ?></h3>
-	<span style="float: right; padding: 5px"><?php echo buttoner(); ?></span>
-	<hr class="clear"/>
 	<?php
 	echo _('Current Version') . ': ' . $current_version . '<br/>';
 	echo ($new_versions ? _('Latest Version Available') . ': ' . ($new_versions[0]->name) : _('You have the latest version of FoOlFuuka.')) . '<br/><br/>';
 	?>
 	
-</div>
 
 <br/>
 

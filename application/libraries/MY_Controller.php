0ec1a43d9ff96a5d02618d83402baa4f7a64bcee
woxxy <woxxy@foolrulez.org>
2012-05-10 22:18:56 +0200

    removing unused assets

--- application/libraries/MY_Controller.php
+++ application/libraries/MY_Controller.php
@@ -111,10 +111,14 @@ class MY_Controller extends CI_Controller
 				$uri_array);
 		}
 		
+		/*
+		 * @todo figure out if this was necessary: it currently goes running functions 
+		 * from routes that sent here because the function didn't exist in first place
 		if (method_exists($this, $method))
 		{
 			return call_user_func_array(array($this, $method), $params);
 		}
+		*/
 		
 		return FALSE;
 	}

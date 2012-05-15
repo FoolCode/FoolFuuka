dbcaeb34e102f89b0c6a19c3ef9aae4c9274d473
woxxy <woxxy@foolrulez.org>
2012-05-15 19:18:19 +0200

    the FU_Nginx_Cache_Purge plugin wasn't actually pushed

---
+++ content/plugins/FU_Nginx_Cache_Purge/FU_Nginx_Cache_Purge.php
@@ -0,0 +1,148 @@
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+
+class FU_Nginx_Cache_Purge extends Plugins_model
+{
+	function __construct()
+	{
+		// KEEP THIS EMPTY, use the initialize_plugin method instead
+
+		parent::__construct();
+	}
+	
+	
+	function structure()
+	{
+		return array(
+			'open' => array(
+				'type' => 'open',
+			),
+			'fu_plugins_nginx_cache_purge_urls' => array(
+				'type' => 'textarea',
+				'preferences' => TRUE,
+				'label' => 'Cache cleaning URLs',
+				'help' => __('Insert the URLs that Nginx Cache Purge will have to contact and their eventual Basic Auth passwords. Make sure you "allow" only the IP from this server on the Nginx Cache Purge configuration block. The following is the format:') . 
+				'<pre style="margin-top:8px">http://0-cdn-archive.yourdomain.org/purge/:username1:yourpass
+http://1-cdn-archive.yourdomain.org/purge/
+http://2-cdn-archive.yourdomain.org/purge/:username2:password</pre>',
+				'class' => 'span8',
+				'validation' => 'trim'
+			),
+			'separator-2' => array(
+				'type' => 'separator-short'
+			),
+			'submit' => array(
+				'type' => 'submit',
+				'class' => 'btn-primary',
+				'value' => __('Submit')
+			),
+			'close' => array(
+				'type' => 'close'
+			),
+		);
+	}
+
+	function initialize_plugin()
+	{
+		$this->plugins->register_controller_function($this,
+			array('admin', 'plugins', 'nginx_cache_purge'), 'manage');
+
+		$this->plugins->register_admin_sidebar_element('plugins',
+			array(
+				"content" => array(
+					"nginx_cache_purge" => array("level" => "admin", "name" => __("Nginx Cache Purge"), "icon" => 'icon-leaf'),
+				)
+			)
+		);
+		
+		$this->plugins->register_hook($this, 'fu_post_model_after_delete_media', 5, 'after_delete_media');
+	}
+
+
+	function manage()
+	{
+		$this->viewdata['controller_title'] = '<a href="' . site_url("admin/plugins/nginx_cache_purge") . '">' . __("Nginx Cache Purge") . '</a>';
+		$this->viewdata['function_title'] = __('Manage');
+
+		if($this->input->post())
+		{
+			set_setting('fu_plugins_nginx_cache_purge_urls', $this->input->post('fu_plugins_nginx_cache_purge_urls'));
+		}
+		
+		$data['form'] = $this->structure();
+		
+		$this->viewdata["main_content_view"] = $this->load->view("admin/form_creator.php", $data, TRUE);
+		$this->load->view("admin/default.php", $this->viewdata);
+	}
+	
+	
+	function after_delete_media($board, $post, $media, $thumb)
+	{
+		$dir = array();
+		if($media)
+		{
+			$dir['full'] = $this->post->get_media_link($board, $post, FALSE, TRUE);
+		}
+		
+		if($thumb)
+		{
+			$dir['thumb'] = $this->post->get_media_link($board, $post, TRUE, TRUE);
+		}
+		
+		$url_user_password = $this->parse_urls();
+		
+		// Phil Sturgeon's curl library
+		$this->load->library('curl');
+		
+		foreach($url_user_password as $item)
+		{
+			foreach($dir as $d)
+			{
+				$this->curl->create($item['url'] . $d);
+				if(isset($item['password']))
+					$this->curl->http_login($item['username'], $item['password']);
+				$this->curl->execute();
+			}
+		}
+	}
+	
+	
+	function parse_urls()
+	{
+		$text = get_setting('fu_plugins_nginx_cache_purge_urls');
+		
+		if(!$text)
+		{
+			return array();
+		}
+		
+		$lines = preg_split('/\r\n|\r|\n/', $text);
+		
+		$lines_exploded = array();
+		
+		foreach($lines as $key => $line)
+		{
+			$explode = explode(':', $line);
+
+			if(count($explode) == 0)
+			{
+				continue;
+			}
+			
+			if(count($explode) > 1)
+				$lines_exploded[$key]['url'] = rtrim(array_shift($explode) . array_shift($explode), '/');
+			
+			if(count($explode) > 1)
+			{
+				$lines_exploded[$key]['username'] = array_shift($explode);
+				$lines_exploded[$key]['password'] = implode(':', $explode);
+			}
+		}
+		
+		return $lines_exploded;
+	}
+
+
+}
---
+++ content/plugins/FU_Nginx_Cache_Purge/FU_Nginx_Cache_Purge_info.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+/*
+ * Here you should complete the required fields
+ */
+
+// class name case sensitive
+$info['slug'] = 'FU_Nginx_Cache_Purge';
+// name under which to display your plugin
+$info['name'] = 'Nginx Cache Purge';
+// short description about what the plugin does
+$info['description'] = sprintf(
+	__('Remove deleted images from the Nginx proxy cache when deleted locally. Requires Nginx compiled with %s to work.'), 
+	'<a href="https://github.com/FRiCKLE/ngx_cache_purge" target="_blank">ngx_cache_purge</a>'
+);
+
+// version in x.x.x format, important for plugin core files upgrade
+$info['version'] = '0.7.0';
+// database revision in number format, important for database schema changes
+$info['revision'] = '0';
+
+// various info
+$info['author'] = 'Woxxy';

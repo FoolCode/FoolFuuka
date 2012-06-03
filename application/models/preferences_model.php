7edb504a15044c7291b1e04b0de0f755dc4f1c6e
woxxy <woxxy@foolrulez.org>
2012-06-03 19:13:29 +0200

    made preferences model plugginable

--- application/models/preferences_model.php
+++ application/models/preferences_model.php
@@ -20,9 +20,73 @@ class Preferences_model extends CI_Model
 	/**
 	 * Nothing special here 
 	 */
-	function __construct()
+	public function __construct()
 	{
 		parent::__construct();
+	}
+	
+	
+	/**
+	 * The functions with 'p_' prefix will respond to plugins before and after
+	 *
+	 * @param string $name
+	 * @param array $parameters
+	 */
+	public function __call($name, $parameters)
+	{
+		$before = $this->plugins->run_hook('fu_preferences_model_before_' . $name, $parameters);
+
+		if (is_array($before))
+		{
+			// if the value returned is an Array, a plugin was active
+			$parameters = $before['parameters'];
+		}
+
+		// if the replace is anything else than NULL for all the functions ran here, the 
+		// replaced function wont' be run
+		$replace = $this->plugins->run_hook('fu_preferences_model_replace_' . $name, $parameters, array($parameters));
+
+		if($replace['return'] !== NULL)
+		{
+			$return = $replace['return'];
+		}
+		else
+		{
+			switch (count($parameters)) {
+				case 0:
+					$return = $this->{'p_' . $name}();
+					break;
+				case 1:
+					$return = $this->{'p_' . $name}($parameters[0]);
+					break;
+				case 2:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1]);
+					break;
+				case 3:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1], $parameters[2]);
+					break;
+				case 4:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1], $parameters[2], $parameters[3]);
+					break;
+				case 5:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1], $parameters[2], $parameters[3], $parameters[4]);
+					break;
+				default:
+					$return = call_user_func_array(array(&$this, 'p_' . $name), $parameters);
+				break;
+			}
+		}
+
+		// in the after, the last parameter passed will be the result
+		array_push($parameters, $return);
+		$after = $this->plugins->run_hook('fu_preferences_model_after_' . $name, $parameters);
+
+		if (is_array($after))
+		{
+			return $after['return'];
+		}
+
+		return $return;
 	}
 	
 	
@@ -31,7 +95,7 @@ class Preferences_model extends CI_Model
 	 * 
 	 * @param array $data name => value
 	 */
-	function submit($data)
+	private function p_submit($data)
 	{
 		foreach ($data as $name => $value)
 		{
@@ -75,7 +139,7 @@ class Preferences_model extends CI_Model
 	 * 
 	 * @param array $form 
 	 */
-	function submit_auto($form)
+	private function p_submit_auto($form)
 	{
 		if ($this->input->post())
 		{

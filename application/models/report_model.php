e579e71e9bd0ddee9069df7eba1bc04fdbcd9904
woxxy <woxxy@foolrulez.org>
2012-06-03 20:03:31 +0200

    completing report model comments and making plugginable

--- application/models/report_model.php
+++ application/models/report_model.php
@@ -3,18 +3,101 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
-
+/**
+ * FoOlFuuka Members Model
+ *
+ * The Reports Model deals with the reports by the users on the
+ * posts in the boards
+ *
+ * @package        	FoOlFrame
+ * @subpackage    	FoOlFuuka
+ * @category    	Models
+ * @author        	FoOlRulez
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html
+ */
 class Report_model extends CI_Model
 {
 
-
+	/**
+	 * Nothing special here 
+	 */
 	function __construct()
 	{
 		parent::__construct(NULL);
 	}
 
+	
+	/**
+	 * The functions with 'p_' prefix will respond to plugins before and after
+	 *
+	 * @param string $name
+	 * @param array $parameters
+	 */
+	public function __call($name, $parameters)
+	{
+		$before = $this->plugins->run_hook('fu_report_model_before_' . $name, $parameters);
 
-	function structure()
+		if (is_array($before))
+		{
+			// if the value returned is an Array, a plugin was active
+			$parameters = $before['parameters'];
+		}
+
+		// if the replace is anything else than NULL for all the functions ran here, the 
+		// replaced function wont' be run
+		$replace = $this->plugins->run_hook('fu_report_model_replace_' . $name, $parameters, array($parameters));
+
+		if ($replace['return'] !== NULL)
+		{
+			$return = $replace['return'];
+		}
+		else
+		{
+			switch (count($parameters))
+			{
+				case 0:
+					$return = $this->{'p_' . $name}();
+					break;
+				case 1:
+					$return = $this->{'p_' . $name}($parameters[0]);
+					break;
+				case 2:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1]);
+					break;
+				case 3:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1], $parameters[2]);
+					break;
+				case 4:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1], $parameters[2], $parameters[3]);
+					break;
+				case 5:
+					$return = $this->{'p_' . $name}($parameters[0], $parameters[1], $parameters[2], $parameters[3], $parameters[4]);
+					break;
+				default:
+					$return = call_user_func_array(array(&$this, 'p_' . $name), $parameters);
+					break;
+			}
+		}
+
+		// in the after, the last parameter passed will be the result
+		array_push($parameters, $return);
+		$after = $this->plugins->run_hook('fu_report_model_after_' . $name, $parameters);
+
+		if (is_array($after))
+		{
+			return $after['return'];
+		}
+
+		return $return;
+	}
+	
+
+	/**
+	 * The structure of a report
+	 * 
+	 * @return array the structure 
+	 */
+	private function p_structure()
 	{
 		return array(
 			'open' => array(
@@ -155,7 +238,13 @@ class Report_model extends CI_Model
 	}
 	
 	
-	function get_reports($page)
+	/**
+	 * Get reports by page
+	 * 
+	 * @param int $page the page
+	 * @return array the rows fetched
+	 */
+	private function p_get_reports($page)
 	{
 		$query = $this->db->limit(25, $page * 25 - 25)->get('reports');
 		if($query->num_rows == 0)
@@ -166,7 +255,13 @@ class Report_model extends CI_Model
 		return $query->result();
 	}
 
-	function get_count()
+	
+	/**
+	 * Return the count of the reports
+	 * 
+	 * @return int the count of the reports
+	 */
+	private function p_get_count()
 	{
 		$query = $this->db->select('COUNT(*) AS count')->get('reports');
 		
@@ -180,7 +275,14 @@ class Report_model extends CI_Model
 		return $row->count;
 	}
 
-	function add($data = array())
+	
+	/**
+	 * Add a report to a post
+	 * 
+	 * @param array $data the data to validate against the structure
+	 * @return array the result from form_validation 
+	 */
+	private function p_add($data = array())
 	{
 		// if it's set we want to know the IP address of the reporter
 		if($this->input->ip_address())
@@ -202,16 +304,30 @@ class Report_model extends CI_Model
 	}
 
 
-	function remove($id)
+	/**
+	 * Remove the report by ID
+	 * 
+	 * @param int $id the ID of the report
+	 */
+	private function p_remove($id)
 	{
 		$this->db->where('id', $id)->delete('reports');
 	}
 
 
-	function remove_by_doc_id($board, $doc_id)
+	/**
+	 * Remove report by doc_id, also multiple reports in case
+	 * 
+	 * @param object $board the board object
+	 * @param int $doc_id the doc_id
+	 */
+	private function p_remove_by_doc_id($board, $doc_id)
 	{
 		$this->db->where('board_id', $board->id)
 			->where('doc_id', $doc_id)->delete('reports');
 	}
 
 }
+
+/* End of file report_model.php */
+/* Location: ./application/models/report_model.php */

69f00ab55d612a8be5a70e2c67492503fa073944
woxxy <woxxy@foolrulez.org>
2012-06-12 17:03:41 +0200

    Created FU_Patch_SOPA_Spoilers_2012 plugin (pushing the files this time)

---
+++ content/plugins/FU_Patch_SOPA_Spoilers_2012/FU_Patch_SOPA_Spoilers_2012_info.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+/*
+ * Here you should complete the required fields
+ */
+
+// class name case sensitive
+$info['slug'] = 'FU_Patch_SOPA_Spoilers_2012';
+// name under which to display your plugin
+$info['name'] = 'Patch SOPA Spoilers 2012';
+// short description about what the plugin does
+$info['description'] = __('Prevents the display of the spoilers used by 4chan during the SOPA protest.');
+
+// version in x.x.x format, important for plugin core files upgrade
+$info['version'] = '0.7.5';
+// database revision in number format, important for database schema changes
+$info['revision'] = '0';
+
+// various info
+$info['author'] = 'FoOlRulez';

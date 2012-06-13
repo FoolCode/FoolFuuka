2a2ce4bf8e025e0a80df28225b77a635aa378a3e
woxxy <woxxy@foolrulez.org>
2012-06-13 23:30:57 +0200

    Initial newhtml yotsuba theme

---
+++ content/themes/yotsuba/views/tools_reply_box.php
@@ -0,0 +1,76 @@
+<hr class="abovePostForm"/>
+	
+	<!--
+	
+	<hr/>
+	
+	<div class="mobilePostFormToggle mobile hidden" id="mpostform" onclick="toggleMobilePostForm();">
+		<div>Show the Posting Form</div>
+	</div>
+	
+	<div style='position:relative'></div>
+	<form name="post" action="https://sys.4chan.org/htmlnew/post" method="post" enctype="multipart/form-data">
+	<input type="hidden" name="MAX_FILE_SIZE" value="3145728">
+	<input type="hidden" name="mode" value="regist"/><table class="postForm hideMobile" id="postForm">
+<tbody>
+<tr>
+<td>Name</td>
+<td><input name="name" type="text"/></td>
+</tr>
+<tr>
+<td>E-mail</td>
+<td><input name="email" type="text"/></td>
+</tr>
+<tr>
+<td>Subject</td>
+<td><input name="sub" type="text"/><input type="submit" value="Submit"/>[<label><input type="checkbox" name="spoiler" value="on">Spoiler Image?</label>]</td>
+</tr>
+<tr>
+<td>Comment</td>
+<td><textarea name="com" cols="48" rows="4" wrap="soft"></textarea></td>
+</tr>
+<tr>
+<td>Verification</td>
+<td> <div>
+<style type="text/css" scoped="scoped">.recaptchatable{background-color:transparent!important;border:none!important;}.recaptcha_image_cell{background-color:transparent!important;}#recaptcha_response_field{border:1px solid #AAA!important;}#recaptcha_div{height:107px;width:442px;}#recaptcha_challenge_field{width:400px}</style>
+<script type="text/javascript">
+		var RecaptchaOptions = {
+			theme : 'clean'
+		};
+		</script>
+<script type="text/javascript" src="//www.google.com/recaptcha/api/challenge?k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc">
+		</script>
+<noscript>
+<iframe src="//www.google.com/recaptcha/api/noscript?k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc" height="300" width="400" style="border: none"></iframe><br>
+<textarea id="recaptcha_challenge_field" name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+<input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+</noscript>
+</div></td>
+</tr>
+<tr>
+<td>File</td>
+<td><input id="postFile" name="upfile" type="file"/>
+</tr>
+<tr>
+<td>Password</td>
+<td><input id="postPassword" name="pwd" type="password"/> <span style="font-size: smaller;">(Password used for file deletion)</span></td>
+</tr>
+<tr class="rules">
+<td colspan="2">
+<ul class="rules">
+<li>Supported file types are: GIF, JPG, PNG</li>
+<li>Maximum file size allowed is 3072 KB.</li>
+<li>Images greater than 250x250 pixels will be thumbnailed.</li>
+<li>Read the <a href="//www.4chan.org/rules#test">rules</a> and <a href="//www.4chan.org/faq">FAQ</a> before posting.</li>
+<li><img src="//static.4chan.org/image/jpn-flag.gif" width="17" height="11" alt="Japanese"> <a href="//www.4chan.org/japanese">このサイトについて</a> -
+<a href="http://honyaku.nifty.com/">翻訳</a></li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>
+</form>
+<script>with(document.post) {name.value=get_cookie("4chan_name"); email.value=get_cookie("4chan_email"); pwd.value=get_pass("4chan_pass"); }</script>
+
+	
+	-->

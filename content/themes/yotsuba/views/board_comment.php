dff0383d4baa2492b75c9c1fe9e38b6ee05c02c9
Hung Tran <oohnoitz@gmail.com>
2012-06-13 17:35:28 -0500

    Updated yotsuba theme to basic support.

--- content/themes/yotsuba/views/board_comment.php
+++ content/themes/yotsuba/views/board_comment.php
@@ -1,29 +1,51 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<div class="postContainer replyContainer" id="pc1440">
-		<div class="sideArrows" id="sa1440">&gt;&gt;</div>
-		<div id="p1440" class="post reply">
-			<div class="postInfo" id="pi1440">
-			<input type="checkbox" name="1440" value="delete"/>
-			<span class="userInfo">
-			<span class="subject">this is a subject</span>
-			<span class="nameBlock capcodeMod">
-			<a href="mailto:an@email.com" class="useremail"><span class="name">name</span> <span class="postertrip">!!xLSZx03dYcu</span> <strong class="capcode">## Mod</strong></a> <img src="//static.4chan.org/image/modicon.gif" alt="This user is a 4chan Moderator." title="This user is a 4chan Moderator." class="identityIcon">
-			</span>
-			<span class="postNum mobile">
-			<a href="res/1418#p1440" title="Highlight this post">No.</a><a href="res/1418#q1440" title="Quote this post">1440</a>
-			</span>
-			</span>
-			<span class="dateTime">04/27/12(Fri)23:15:13</span>
-			<span class="postNum desktop">
-			<a href="res/1418#p1440" title="Highlight this post">No.</a><a href="res/1418#q1440" title="Quote this post">1440</a>
-			</span>
+
+<?php
+$selected_radix = isset($p->board)?$p->board:get_selected_radix();
+
+$num =  $p->num . ( $p->subnum ? '_' . $p->subnum : '' );
+$quote_mode = 'thread';
+?>
+
+<div class="postContainer replyContainer" id="pc<?= $num ?>">
+	<div class="sideArrows" id="sa<?= $num ?>">&gt;&gt;</div>
+		<div id="p<?= $num ?>" class="post reply">
+			<div class="postInfo" id="pi<?= $num ?>">
+				<input type="checkbox" name="<?= $num ?>" value="delete"/>
+				<span class="userInfo">
+					<span class="subject"><?= $p->title_processed ?></span>
+					<span class="nameBlock<?= (($p->capcode == 'M') ? ' capcodeMod':'') . (($p->capcode == 'A') ? ' capcodeAdmin':'') ?>">
+						<span class="name"><?= ($p->email_processed && $p->email_processed != 'noko') ? '<a href="mailto:' . form_prep($p->email_processed) . '">' . $p->name_processed . '</a>' : $p->name_processed ?></span>
+						<?php if ($p->trip) : ?><span class="postertrip"><?= $p->trip ?></span><?php endif; ?>
+						<?php if (in_array($p->capcode, array('M', 'A'))) : ?>
+							<strong class="capcode">## <?= (($p->capcode == 'M') ? 'Mod':'') . (($p->capcode == 'A') ? 'Admin':'') ?>"></strong>
+							<?php if ($p->capcode == 'M') : ?><img src="//static.4chan.org/image/modicon.gif" alt="This user is a Moderator." title="This user is a Moderator." class="identityIcon" style="float: none!important; margin-left: 0px;"><?php endif; ?>
+							<?php if ($p->capcode == 'A') : ?><img src="//static.4chan.org/image/adminicon.gif" alt="This user is an Administrator." title="This user is an Administrator." class="identityIcon" style="float: none!important; margin-left: 0px;"><?php endif; ?>
+						<?php endif; ?>
+					</span>
+					<span class="postNum mobile">
+						<a href="<?= site_url(array($selected_radix->shortname, 'thread', $p->thread_num)) ?>#p<?= $num ?>" title="Highlight this post">No.</a><a href="<?= site_url(array($selected_radix->shortname, $quote_mode, $p->thread_num)) ?>#q<?= $num ?>" title="Quote this post"><?= str_replace('_', ',', $num) ?></a>
+					</span>
+				</span>
+				<span class="dateTime"><?= date('D M d H:i:s Y', $p->original_timestamp) ?></span>
+				<span class="postNum desktop">
+					<a href="<?= site_url(array($selected_radix->shortname, 'thread', $p->thread_num)) ?>#p<?= $num ?>" title="Highlight this post">No.</a><a href="<?= site_url(array($selected_radix->shortname, $quote_mode, $p->thread_num)) ?>#q<?= $num ?>" title="Quote this post"><?= str_replace('_', ',', $num) ?></a>
+				</span>
 			</div>
-			<div class="file" id="f1440">
-			<div class="fileInfo">
-			<span class="fileText">File: <a href="//images.4chan.org/htmlnew/src/1335582913205.png" target="_blank">1335582913.png</a>-(10 KB, 870x582, <span title="10.png">10.png</span>)</span>
+
+			<?php if ($p->preview_orig) : ?>
+			<div class="file" id="f<?= $num ?>">
+				<div class="fileInfo">
+						<span class="fileText">
+							<?= __('File:') ?>
+							<a href="<?= ($p->media_link) ? $p->media_link : $p->remote_media_link ?>" target="_blank"><?= $p->media_filename_processed ?></a>-(<?= byte_format($p->media_size, 0) . ', ' . $p->media_w . 'x' . $p->media_h . ', ' . $p->media_filename_processed ?>)
+						</span>
+				</div>
+				<a class="fileThumb" href="<?= ($p->media_link) ? $p->media_link : $p->remote_media_link ?>" target="_blank">
+					<img src="<?= $p->thumb_link ?>" alt="<?= byte_format($p->media_size, 0) ?>" data-md5="<?= $p->media_hash ?>"<?php if ($p->preview_w > 0 && $p->preview_h > 0) : ?> style="height: <?= $p->preview_h ?>px; width: <?= $p->preview_w ?>px;"<?php endif; ?>/>
+				</a>
 			</div>
-			<a class="fileThumb" href="//images.4chan.org/htmlnew/src/1335582913205.png" target="_blank"><img src="//0.thumbs.4chan.org/htmlnew/thumb/1335582913205s.jpg" alt="10 KB" data-md5="sFlI3O2pIbvr95J1QP0X6Q==" style="height: 85px; width: 126px;"/></a>
-			</div>
-			<blockquote class="postMessage" id="m1440">Some comment<br/><span class="quote">&gt;some quote</span><br/><span class="quote"><a href="res/1418#p1431" class="quotelink">&gt;&gt;1431</a> some link</span><br/><span class="quote"><a href="res/1418#p1418" class="quotelink">&gt;&gt;1418</a> some op link</span><br/><span class="spoiler" onmouseover="this.style.color='#FFF';" onmouseout="this.style.color=this.style.backgroundColor='#000'" style="color:#000;background:#000">some spoiler</span><br/><span class="quote">&gt;&gt;1 some dead link</span></blockquote>
-		</div>
+			<?php endif; ?>
+			<blockquote class="postMessage" id="m<?= $num ?>"><?= $p->comment_processed ?></blockquote>
 	</div>
+</div>

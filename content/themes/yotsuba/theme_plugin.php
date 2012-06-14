0ce7db32440c67560a4b1e301e15a285e16d64e9
Hung Tran <oohnoitz@gmail.com>
2012-06-14 00:48:06 -0500

    Added theme_plugin file for yotsuba.

---
+++ content/themes/yotsuba/theme_plugin.php
@@ -0,0 +1,80 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+
+class Theme_Plugin_yotsuba extends Plugins_model
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+
+	function initialize_plugin()
+	{
+		// use hooks for manipulating comments
+		$this->plugins->register_hook($this, 'fu_post_model_process_comment_greentext_result', 8, '_greentext');
+		$this->plugins->register_hook($this, 'fu_post_model_process_internal_links_html_result', 8,
+			'_process_internal_links_html');
+
+		$this->plugins->register_hook($this, 'fu_post_model_process_crossboard_links_html_result', 8,
+			'_process_crossboard_links_html');
+
+		$this->plugins->register_hook($this, 'fu_chan_controller_before_page', 3, 'page');
+		$this->plugins->register_hook($this, 'fu_chan_controller_before_gallery', 3, function(){ show_404(); });
+		// for safety, force 404
+		$this->plugins->register_hook($this, 'fu_chan_controller_before_submit', 3, function(){ show_404(); });
+
+		// if we have to outright change the name of the function, we need to register a new controller function
+		$this->plugins->register_controller_function($this, array('chan', '(:any)', 'sending'), 'sending');
+	}
+
+
+	function _greentext($html)
+	{
+		return '\\1<span class="quote">\\2</span>\\3';
+	}
+
+	function _process_internal_links_html($data, $html, $previous_result = NULL)
+	{
+		// a plugin with higher priority modified this
+		if(!is_null($previous_result))
+		{
+			return array('return' => $previous_result);
+		}
+
+		return array('return' => array(
+			'tags' => array('<span class="quote">', '</span>'),
+			'hash' => '',
+			'attr' => 'class="quotelink"',
+			'attr_op' => 'class="backlink"',
+			'attr_backlink' => 'class="backlink"',
+		));
+	}
+
+	function _process_crossboard_links_html($data, $html, $previous_result = NULL)
+	{
+		// a plugin with higher priority modified this
+		if(!is_null($previous_result))
+		{
+			return array('return' => $previous_result);
+		}
+
+		return array('return' => array(
+			'tags' => array('<span class="unkfunc">', 'suffix' => '</span>'),
+			'backlink' => ''
+		));
+	}
+
+	/**
+	 * @param int $page
+	 */
+	public function page($page = 1)
+	{
+		return array('parameters' => array($page, FALSE, array('per_page' => 24, 'type' => 'by_thread')));
+	}
+
+}

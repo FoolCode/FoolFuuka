b0021aead7cc13e0cf9b7b1d53570e89ce9e31e4
woxxy <woxxy@foolrulez.org>
2012-07-26 00:44:59 +0200

    adding admin panel in foolfuuka autoroutes

--- config/autoroutes.php
+++ config/autoroutes.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	'_root_'  => 'foolfuuka/chan/index',  // The default route
-	'admin/(:any)' => 'admin/$1',
+	'admin/boards/(:any)' => 'foolfuuka/admin/boards/$1',
 	'search/(:any)' => 'foolfuuka/chan/search',
 	'(?!(admin|api|content|assets|search))(\w+)' => 'foolfuuka/chan/$2/page',
 	'(?!(admin|api|content|assets|search))(\w+)/(:any)' => 'foolfuuka/chan/$2/$3',

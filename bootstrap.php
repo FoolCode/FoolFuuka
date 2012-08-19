cce992ffd3db9510b959092fa7172ea03cfa6682
woxxy <woxxy@foolrulez.org>
2012-08-19 22:48:54 +0200

    Moving sphinxql to a package

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,12 @@
 	'Foolfuuka\\Model\\Board' => APPPATH.'modules/foolfuuka/classes/model/board.php',
 	'Foolfuuka\\Model\\Comment' => APPPATH.'modules/foolfuuka/classes/model/comment.php',
 	'Foolfuuka\\Model\\Media' => APPPATH.'modules/foolfuuka/classes/model/media.php',
-	'Foolfuuka\\Tasks\\Fool' => APPPATH.'modules/foolfuuka/classes/task/fool.php'
+	'Foolfuuka\\Tasks\\Fool' => APPPATH.'modules/foolfuuka/classes/task/fool.php',
 ));
 
 \Autoloader::add_core_namespace('Foolfuuka\\Model');
-
+\Package::load('sphinxql');
 \Package::load('stringparser-bbcode', APPPATH.'modules/foolfuuka/packages/stringparser-bbcode/');
-\Package::load('sphinxql', APPPATH.'modules/foolfuuka/packages/sphinxql/');
 
 $theme = \Theme::forge('foolfuuka');
 $theme->set_module('foolfuuka');
--- packages/sphinxql/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-\Autoloader::add_classes(array(
-    'Foolz\\Sphinxql\\Sphinxql' => __DIR__.'/classes/model/sphinxql.php',
-));

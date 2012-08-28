fc7c3a7931209c82754c8ee3d5ef7443bde05fa4
woxxy <woxxy@foolrulez.org>
2012-08-28 20:06:40 +0200

    Foolauth file specific for model

---
+++ config/foolauth.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	'roles' => array(
+		'user' => array('access' => array('user', 'member'), 'maccess' => array('user')),
+		'mod' => array(
+			'access' => array('mod'),
+			'maccess' => array('user', 'mod'),
+			'comment' => array('see_ip', 'passwordless_deletion', 'limitless_comment', 'reports', 'mod_capcode', 'dev_capcode'),
+			'users' => array('access')
+		),
+		'admin' => array(
+			'access' => array('admin'),
+			'maccess' => array('user', 'mod', 'admin'),
+			'boards' => array('edit'),
+			'comment' => array('see_ip', 'passwordless_deletion', 'limitless_comment', 'reports', 'mod_capcode', 'admin_capcode', 'dev_capcode'),
+			'users' => array('access', 'change_credentials', 'change_group')
+		),
+	),
+);

d838ee5a537d6e7de235f00b3f67386a35b7be95
woxxy <woxxy@foolrulez.org>
2012-09-14 19:47:56 +0200

    used wrong operators

--- themes/default/views/board.php
+++ themes/default/views/board.php
@@ -10,7 +10,7 @@ foreach ($board->get_comments() as $key => $post) :
 <article id="<?= $num ?>" class="clearfix thread doc_id_<?= $op->doc_id ?> board_<?= $op->board->shortname ?>">
 
 
-	<?php if (isset($disable_default_after_op_open) && $disable_default_after_op_open === true) : ?>
+	<?php if ( ! isset($disable_default_after_op_open) || $disable_default_after_op_open !== true) : ?>
 	<?php \Plugins::run_hook('fu.themes.default_after_op_open', array($op->board)); ?>
 	<?php endif; ?>
 
@@ -143,7 +143,7 @@ foreach ($board->get_comments() as $key => $post) :
 <?php elseif (isset($post['posts'])): ?>
 <article class="clearfix thread">
 
-	<?php if (isset($disable_default_after_headless_open) && $disable_default_after_headless_open === true) : ?>
+	<?php if ( ! isset($disable_default_after_headless_open) || $disable_default_after_headless_open !== true) : ?>
 	<?php \Plugins::run_hook('fu.themes.default_after_headless_open', array(isset($radix) ? $radix : null)); ?>
 	<?php endif; ?>
 

c6f2401af794ca4a17fbab33d1d76062bea2de32
woxxy <woxxy@foolrulez.org>
2012-09-23 16:10:27 +0200

    make board_comment a class

--- themes/default/bootstrap.php
+++ themes/default/bootstrap.php
@@ -2,3 +2,7 @@
 
 if (!defined('DOCROOT'))
 	exit('No direct script access allowed');
+
+\Autoloader::add_classes(array(
+	'Foolfuuka\\Themes\\Default_\\Views\\Board_comment' => __DIR__.'/views/board_comment.php',
+));

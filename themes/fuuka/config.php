c87641f42ca04561448094cedc8c7d1888aea092
Hung Tran <oohnoitz@gmail.com>
2012-09-24 16:05:49 -0500

    Updated layouts to use theme version instead of main version.

--- themes/fuuka/config.php
+++ themes/fuuka/config.php
@@ -4,27 +4,27 @@ if (!defined('DOCROOT'))
 	exit('No direct script access allowed');
 
 return array(
-	
+
 	'directory' => 'fuuka',
-	
+
 	'name' => 'Fuuka',
-	
+
 	'description' => 'The Fuuka theme',
-	
-	'version' => '0.1.0',
-	
+
+	'version' => '0.2.0',
+
 	'author' => 'Foolz',
-	
+
 	'author_link' => 'http://www.foolz.us',
-	
+
 	'support_link' => '',
-	
+
 	'license' => 'Apache License 2.0',
-	
+
 	'license_link' => 'http://www.apache.org/licenses/LICENSE-2.0.html',
-	
+
 	'extends' => 'default',
-	
+
 	'extends_css' => false,
-	
+
 );

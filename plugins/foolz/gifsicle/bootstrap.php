0e8541cce1d3547ba11f0f3b40e03299fc34aaf4
woxxy <woxxy@foolrulez.org>
2012-10-13 23:13:16 +0200

    make gifsicle work only on gifs and only if the result is void

--- plugins/foolz/gifsicle/bootstrap.php
+++ plugins/foolz/gifsicle/bootstrap.php
@@ -9,12 +9,19 @@ if (!defined('DOCROOT'))
 		\Foolz\Plugin\Event::forge('fu.model.media.insert.resize')
 			->setCall(function($result){
 
+				if ( ! $result instanceof \Foolz\Plugin\Void || strtolower($this->temp_extension) !== 'gif')
+				{
+					// someone is already done with the image or it's not a gif
+					return;
+				}
+
 				exec("gifsicle ".$result->getParam('full_path')." --colors 256 --color-method blend-diversity"
 					." --resize-fit ".$result->getParam('thumb_width')."x".$result->getParam('thumb_height')
 					." --dither > ".$this->path_from_filename(true, $result->getParam('is_op'), true));
 
 				// only change the result so it's not void
 				$result->set('done');
+
 			});
 
 	});

29359f507b33fde7516db007be7cf189231be41d
woxxy <woxxy@foolrulez.org>
2012-10-13 23:03:06 +0200

    gifsicle plugin is back and it works

---
+++ plugins/foolz/gifsicle/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('DOCROOT'))
+	exit('No direct script access allowed');
+
+\Foolz\Plugin\Event::forge('foolz\plugin\plugin.execute.foolz/gifsicle')
+	->setCall(function($result) {
+
+		\Foolz\Plugin\Event::forge('fu.model.media.insert.resize')
+			->setCall(function($result){
+
+				exec("gifsicle ".$result->getParam('full_path')." --colors 256 --color-method blend-diversity"
+					." --resize-fit ".$result->getParam('thumb_width')."x".$result->getParam('thumb_height')
+					." --dither > ".$this->path_from_filename(true, $result->getParam('is_op'), true));
+
+				// only change the result so it's not void
+				$result->set('done');
+			});
+
+	});

085f3266fb38df68e778582efa7cabaadccc0397
woxxy <woxxy@foolrulez.org>
2013-01-04 01:11:26 +0100

    Reinputting the plugin directories

---
+++ public/plugins/foolz/foolfuuka-plugin-image-in-html/bootstrap.php
@@ -0,0 +1,40 @@
+<?php
+
+\Foolz\Plugin\Event::forge('Foolz\Plugin\Plugin::execute.foolz/foolfuuka-plugin-image-in-html')
+	->setCall(function($result) {
+		\Autoloader::add_classes(array(
+			'Foolfuuka\\Plugins\\Image_In_Html\\Controller_Plugin_Fu_Image_In_Html_Chan'
+				=> __DIR__.'/classes/controller/chan.php'
+		));
+
+		\Router::add('(?!(admin|_))(\w+)/image_html/(:any)', 'plugin/fu/image_in_html/chan/$2/image_html/$3', true);
+
+
+		\Foolz\Plugin\Event::forge('foolfuuka\\model\\media.getLink.call.before')
+			->setCall(function($result) {
+
+				$element = $result->getObject();
+				list($thumbnail, $direct) = $result->getParam();
+
+				if ($direct === true || $thumbnail === true)
+				{
+					return;
+				}
+
+				// this function must NOT run for the radix_full_image function
+				if (\Radix::getByShortname(\Uri::segment(1)) && \Uri::segment(2) === 'full_image')
+				{
+					return;
+				}
+
+				$return = $element->p_getLink($thumbnail);
+				if ($return === null)
+				{
+					return;
+				}
+
+				$result->set(\Uri::create(array($element->board->shortname, 'image_html')).$element->media);
+			})->setPriority(4);
+	});
+
+

09ebf647e31af73b9ed51b053ba157b330af4a70
Hung Tran <oohnoitz@gmail.com>
2013-01-25 18:05:05 -0600

    added reports counter in admin sidebar

--- config/sidebar.php
+++ config/sidebar.php
@@ -1,28 +1,51 @@
 <?php
 
 return [
-
 	'sidebar' => [
-
 		'boards' => [
-			"name" => __("Boards"),
-			"level" => "admin",
-			"default" => "manage",
-			"content" => [
-				"manage" => ["alt_highlight" => ["board"],
-					"level" => "admin", "name" => __("Manage"), "icon" => 'icon-th-list'],
-				"search" => ["level" => "admin", "name" => __("Search"), "icon" => 'icon-search'],
-				"preferences" => ["level" => "admin", "name" => __("Preferences"), "icon" => 'icon-check']
+			'name' => __('Boards'),
+			'level' => 'admin',
+			'default' => 'manage',
+			'content' => [
+				'manage' => [
+					'alt_highlight' => ['board'],
+					'level' => 'admin',
+					'name' => __('Manage'),
+					'icon' => 'icon-th-list'
+				],
+				'search' => [
+					'level' => 'admin',
+					'name' => __('Search'),
+					'icon' => 'icon-search'
+				],
+				'preferences' => [
+					'level' => 'admin',
+					'name' => __('Preferences'),
+					'icon' => 'icon-check'
+				]
 			]
 		],
 		'posts' => [
-			"name" => __("Posts"),
-			"level" => "mod",
-			"default" => "reports",
-			"content" => [
-				"reports" => ["level" => "mod", "name" => __("Reports"), "icon" => 'icon-tag'],
-				"bans" => ["level" => "mod", "name" => __("Bans"), "icon" => 'icon-truck'],
-				"appeals" => ["level" => "mod", "name" => __("Pending Appeals"), "icon" => 'icon-heart-empty'],
+			'name' => __('Posts'),
+			'level' => 'mod',
+			'default' => 'reports',
+			'content' => [
+				'reports' => [
+					'level' => 'mod',
+					'name' => __('Reports'),
+					'icon' => 'icon-tag',
+					'notification' => \Report::count()
+				],
+				'bans' => [
+					'level' => 'mod',
+					'name' => __('Bans'),
+					'icon' => 'icon-truck'
+				],
+				'appeals' => [
+					'level' => 'mod',
+					'name' => __('Pending Appeals'),
+					'icon' => 'icon-heart-empty'
+				]
 			]
 		]
 	]

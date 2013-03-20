3fcdcd1b456bb6932e18de182919a29f7b2e6ca2
woxxy <woxxy@foolrulez.org>
2013-03-20 08:00:06 +0100

    Formatting help in Nginx Cache Purge plugin

--- public/plugins/foolz/foolfuuka-nginx-cache-purge/classes/controller/admin.php
+++ public/plugins/foolz/foolfuuka-nginx-cache-purge/classes/controller/admin.php
@@ -28,8 +28,8 @@ class NginxCachePurge extends \Foolz\Foolframe\Controller\Admin
 				'label' => __('Cache cleaning URLs'),
 				'help' => __('Insert the URLs that Nginx Cache Purge will have to contact and their eventual Basic Auth passwords. Make sure you "allow" only the IP from this server on the Nginx Cache Purge configuration block. The following is the format:') .
 				'<pre style="margin-top:8px">http://0-cdn-archive.yourdomain.org/purge/:username1:yourpass
-				http://1-cdn-archive.yourdomain.org/purge/
-				http://2-cdn-archive.yourdomain.org/purge/:username2:password</pre>',
+http://1-cdn-archive.yourdomain.org/purge/
+http://2-cdn-archive.yourdomain.org/purge/:username2:password</pre>',
 				'class' => 'span8',
 				'validation' => 'trim'
 			],

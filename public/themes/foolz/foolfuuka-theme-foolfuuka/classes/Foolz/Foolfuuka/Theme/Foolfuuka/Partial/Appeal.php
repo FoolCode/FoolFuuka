dd3c5bba93e78cde017d9af08f4dda6a9ff812b7
woxxy <woxxap@gmail.com>
2014-05-03 16:45:11 +0200

    Fixing Appeal page display

--- public/themes/foolz/foolfuuka-theme-foolfuuka/classes/Foolz/Foolfuuka/Theme/Foolfuuka/Partial/Appeal.php
+++ public/themes/foolz/foolfuuka-theme-foolfuuka/classes/Foolz/Foolfuuka/Theme/Foolfuuka/Partial/Appeal.php
@@ -12,13 +12,13 @@ class Appeal extends \Foolz\Foolfuuka\View\View
         ?>
             <div class="alert alert-success" style="margin:15% 30%;">
                 <h4 class="alert-heading"><?= e($title) ?></h4>
-                <br/>
+                <br>
                 <?= $form->open(); ?>
                 <?= $form->hidden('csrf_token', $this->getSecurity()->getCsrfToken()); ?>
                 <?= _i('Explain in short (max. 500 chars) why your ban should be lifted.') ?>
-                <br/>
-                <?= $form->textarea('appeal', null, array('style' => 'width: 100%; height: 100px; margin: 10px 0')) ?>
-                <?= $form->submit(array('name' => 'submit', 'value' => _i('Submit'), 'class' => 'btn btn-inverse')) ?>
+                <br>
+                <?= $form->textarea([ 'name' => 'appeal', 'style' => 'width: 100%; height: 100px; margin: 10px 0']) ?>
+                <?= $form->submit(['name' => 'submit', 'value' => _i('Submit'), 'class' => 'btn btn-inverse']) ?>
                 <?= $form->close(); ?>
             </div>
         <?php

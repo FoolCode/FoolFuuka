0ceaa7d2e0b04c286fb7f1dfdaf515be20265eab
Hasumi <hasumi@archive.moe>
2015-01-11 19:59:33 +0000

    added support for searching country data

--- assets/public/themes-admin/foolz/foolfuuka-theme-admin/classes/Foolz/Foolfuuka/Theme/Admin/Partial/Boards/SphinxDistConfig.php
+++ assets/public/themes-admin/foolz/foolfuuka-theme-admin/classes/Foolz/Foolfuuka/Theme/Admin/Partial/Boards/SphinxDistConfig.php
@@ -41,7 +41,7 @@ source template
   sql_range_step = 10000
   sql_query      = \
       SELECT doc_id, <?= $example->id ?> AS board, timestamp, thread_num AS tnum, num, subnum, name, trip, email, title, comment, \
-        media_id as mid, media_filename, media_hash, poster_ip as pip, poster_hash as pid, ASCII(capcode) AS cap, \
+        media_id AS mid, media_filename, media_hash, poster_ip AS pip, poster_hash AS pid, poster_country AS country, ASCII(capcode) AS cap, \
         (subnum != 0) AS is_internal, spoiler AS is_spoiler, deleted AS is_deleted, sticky AS is_sticky, op AS is_op, \
         (media_filename != '' AND media_filename IS NOT NULL) AS has_image \
       FROM `<?= $example->shortname ?>` LIMIT 1
@@ -70,7 +70,7 @@ source <?= $board->shortname.'_ancient0' ?> : template
 {
   sql_query      = \
       SELECT doc_id, <?= $board->id ?> AS board, timestamp, thread_num AS tnum, num, subnum, name, trip, email, title, comment, \
-        media_id as mid, media_filename, media_hash, poster_ip as pip, poster_hash as pid, ASCII(capcode) AS cap, \
+        media_id AS mid, media_filename, media_hash, poster_ip AS pip, poster_hash AS pid, poster_country AS country, ASCII(capcode) AS cap, \
         (subnum != 0) AS is_internal, spoiler AS is_spoiler, deleted AS is_deleted, sticky AS is_sticky, op AS is_op, \
         (media_filename != '' AND media_filename IS NOT NULL) AS has_image \
       FROM <?= $board->getTable() ?> WHERE doc_id >= $start AND doc_id <= $end AND doc_id % <?= $sphinx['distributed'] ?> = 0
@@ -85,7 +85,7 @@ source <?= $board->shortname.'_ancient'.$i ?> : template
 {
   sql_query      = \
       SELECT doc_id, <?= $board->id ?> AS board, timestamp, thread_num AS tnum, num, subnum, name, trip, email, title, comment, \
-        media_id as mid, media_filename, media_hash, poster_ip as pip, poster_hash as pid, ASCII(capcode) AS cap, \
+        media_id AS mid, media_filename, media_hash, poster_ip AS pip, poster_hash AS pid, poster_country AS country, ASCII(capcode) AS cap, \
         (subnum != 0) AS is_internal, spoiler AS is_spoiler, deleted AS is_deleted, sticky AS is_sticky, op AS is_op, \
         (media_filename != '' AND media_filename IS NOT NULL) AS has_image \
       FROM <?= $board->getTable() ?> WHERE doc_id >= $start AND doc_id <= $end AND doc_id % <?= $sphinx['distributed'] ?> = <?= $i.PHP_EOL ?>
@@ -112,7 +112,7 @@ source <?= $board->shortname.'_delta' ?> : template
 {
   sql_query      = \
       SELECT doc_id, <?= $board->id ?> AS board, timestamp, thread_num AS tnum, num, subnum, name, trip, email, title, comment, \
-        media_id as mid, media_filename, media_hash, poster_ip as pip, poster_hash as pid, ASCII(capcode) AS cap, \
+        media_id AS mid, media_filename, media_hash, poster_ip AS pip, poster_hash AS pid, poster_country AS country, ASCII(capcode) AS cap, \
         (subnum != 0) AS is_internal, spoiler AS is_spoiler, deleted AS is_deleted, sticky AS is_sticky, op AS is_op, \
         (media_filename != '' AND media_filename IS NOT NULL) AS has_image \
       FROM <?= $board->getTable() ?> WHERE doc_id >= $start AND doc_id <= $end

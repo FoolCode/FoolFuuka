409186b8305741849e240ca01f1521f2b165e947
anspage® <anspagespace@hotmail.com>
2019-04-01 11:51:02 +0300

    Revert "Add Imgops support."

--- assets/themes/foolz/foolfuuka-theme-fuuka/src/Partial/BoardComment.php
+++ assets/themes/foolz/foolfuuka-theme-fuuka/src/Partial/BoardComment.php
@@ -70,7 +70,6 @@ class BoardComment extends \Foolz\FoolFuuka\View\View
                 <?php if (!$p->radix->hide_thumbnails || $this->getAuth()->hasAccess('maccess.mod')) : ?>
                     [<a href="<?= $this->getUri()->create($p->radix->shortname . '/search/image/' . $p_media->getSafeMediaHash()) ?>"><?= _i('View Same') ?></a>]
                     [<a href="http://google.com/searchbyimage?image_url=<?= $p_media->getThumbLink($this->getRequest()) ?>">Google</a>]
-                    [<a href="http://imgops.com/<?= $p_media->getThumbLink($this->getRequest()) ?>">ImgOps</a>]
                     [<a href="http://iqdb.org/?url=<?= $p_media->getThumbLink($this->getRequest()) ?>">iqdb</a>]
                     [<a href="http://saucenao.com/search.php?url=<?= $p_media->getThumbLink($this->getRequest()) ?>">SauceNAO</a>]
                     <?php endif; ?>
